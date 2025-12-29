3c4f6cd4f2bce7fa503b7702e2d24c2b2859ae4f
Sara <slahrach.me@gmail.com>
2025-12-29 15:52:58 +0100

    Update footer to 'Built with Next.js by Sara'

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className="pb-5 md:pb-7 text-white/55 text-sm text-center">
 			<p>
-				<span className="text-white/75">Next.js</span> by Sara
+				Built with <span className="text-white/75">Next.js</span> by Sara
 			</p>
 		</footer>
 	);
